8236e33e59007c3f4c5ca9c3b087093b59688532
Samanta Fluture <samantafluture@gmail.com>
2021-05-27 19:45:11 -0300

    filtering and mapping customers from api

---
+++ Desktop/clientes/clientes.js
@@ -0,0 +1,71 @@
+console.log("=== CLIENTES ===");
+
+(() => {
+
+/* === UI === */
+
+const ui = {
+    table: document.querySelector("table tbody")
+};
+
+/* === ACTIONS === */
+
+const getCustomers = async () => {
+    const endpoint = "https://run.mocky.io/v3/ea32efab-c290-4fcf-85d9-293fbb84b9cc";
+
+    const config = {
+        method: "GET",
+        headers: new Headers({
+            "Content-type": "application/json"
+        })
+    };
+
+    try {
+        const response = await fetch(endpoint, config);
+        const customers = await response.json();
+        getCustomerSuccess(customers);
+    } catch (error) {
+        getCustomerError(error);
+    }
+};
+
+const getCustomerSuccess = (customers) => {
+  console.table(customers);
+
+  // === IMPLEMENTAÇÃO #2
+
+  
+
+  
+  // === IMPLEMENTAÇÃO #1
+
+  // retorno implícito
+  // só tem 1 parâmetro pode tirar ()
+  // pode tb tirar o return e {}
+
+  // ui.table.innerHTML =
+  //   customers
+  //       .filter(customer => customer.id <= 25) // trago novo array com os elementos filtrados (filter)
+  //       .map(customer => {
+  //         const { name, email, phone, country } = customer; // customer é um objeto; estou extraindo as propriedades que preciso
+  //         return `<tr> 
+  //                   <td>${name}</td>
+  //                   <td>${email}</td>
+  //                   <td>${phone}</td>
+  //                   <td>${country}</td>
+  //                 </tr>`;
+  //       // retorno o html preenchido com os atributos
+  //       // devolve os dados transformados (map)
+  //       // formo um nov array com estes dados transformados
+  //   }).join("")
+  };
+
+const getCustomerError = (error) => {
+    console.table(error);
+};
+
+/* === INITIALIZE === */
+
+getCustomers();
+
+})();
